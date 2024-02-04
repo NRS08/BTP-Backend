6d923fb6e506c51075a34d08912f9ea33d853b00
NRS08 <nirantsontakke@gmail.com>
2024-02-05 01:34:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+require("express-async-errors");
+const connectDB = require("./db/connect");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const authRouter = require("./routes/auth");
+
+app.get("/", (req, res) => {
+  res.send("Insurance Dapp");
+});
+
+app.use("/api/v1/auth", authRouter);
+
+const port = process.env.PORT || 4000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listining on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
